e62915f781a0fa73f0f1ff13a0188a7bcf283858
Andres-Delgado <andres36@mail.usf.edu>
2019-02-04 21:35:03 -0500

    project finished

--- Receiver/app/src/main/java/edu/usf/andres/receiver/MainActivity.java
+++ Receiver/app/src/main/java/edu/usf/andres/receiver/MainActivity.java
@@ -4,6 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,14 +23,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // define the display text view
         TextView txtView = findViewById(R.id.txtDisplay);
 
-        // get the app data send on bundle
-
+        // get the app data sent on bundle
         Bundle b = getIntent().getExtras();
+        byte[] cipherText = b.getByteArray("Comment");
+        //byte[] cipherText = getIntent().getByteArrayExtra("Comment");
 
-        // display the key that has the data
-        txtView.setText(b.getString("Comment"));
+        // create secret
+        String pw = "andres13andres13";
+        final Key secret = new SecretKeySpec(pw.getBytes(), "AES");
+
+        // getting String from bundle
+        //String msg = b.getString("Comment");
+        String msg = "";
+
+        // decrypt string
+        try {
+            msg = DecryptMessage(cipherText, secret);
+
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+
+        txtView.setText(msg);
+    }
+
+
+    public String DecryptMessage(byte[] cipherText, Key secret) throws InvalidKeyException,
+            NoSuchPaddingException, NoSuchAlgorithmException, UnsupportedEncodingException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secret);
+        return new String(cipher.doFinal(cipherText));
     }
 }
--- Sender/app/src/main/java/edu/usf/andres/sender/MainActivity.java
+++ Sender/app/src/main/java/edu/usf/andres/sender/MainActivity.java
@@ -7,6 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -21,18 +31,53 @@ public class MainActivity extends AppCompatActivity {
         // init comment edit text
         final EditText etComment = findViewById(R.id.etComment);
 
+        // create secret
+        String pw = "andres13andres13";
+        final Key secret = new SecretKeySpec(pw.getBytes(), "AES");
+
+
         sendButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 //set the package that we want to run
-                Intent intent = getPackageManager().getLaunchIntentForPackage("edu.usf.andres.Receiver");
+                Intent intent = getPackageManager().getLaunchIntentForPackage("edu.usf.andres.receiver");
+
+                String commentStr = etComment.getText().toString();
+                byte[] cipherText = new byte[0];
+
+                try {
+                    cipherText = EncryptMessage(commentStr, secret);
+                    //commentStr = new String(cipherText);
+
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (InvalidKeyException e) {
+                    e.printStackTrace();
+                } catch (BadPaddingException e) {
+                    e.printStackTrace();
+                } catch (IllegalBlockSizeException e) {
+                    e.printStackTrace();
+                }
 
                 // put the data that we want to send over intent
-                intent.putExtra("Comment", etComment.getText().toString());
+                intent.putExtra("Comment", cipherText);
                 startActivity(intent);
+
 
             }
         });
+
     }
+
+    public byte[] EncryptMessage(String msg, Key secret) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        return cipher.doFinal(msg.getBytes());
+    }
+
 }
